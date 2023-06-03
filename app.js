881806ef0cd009bb4e2d68ccc4096aaafc6e3a05
anumaslam-789 <130720606+anumaslam-789@users.noreply.github.com>
2023-06-03 12:08:47 +0500

    assignment completed

---
+++ app.js
@@ -0,0 +1,156 @@
+//var empty = [];
+//2. Declared an emty array using js object notation to store name in futur
+//var empty = new empty ();
+// declared a number array
+ //let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//document.write(num);
+//declare and initialize a string array
+//var vegetables = ["carrot" ,"bringle"  , "cucumber" , "onion"];
+//document.write("string Array :" + vegetables + "<br/>" + "<br/>" + "<br/>");
+//console.log(vegetables);
+//dceclare and initialize boolen array
+//let bool = [true , false];
+//   6)' MIXED ARRAY'
+
+//let mixed  = [1 , 2 ," apple" , "mango" ,true, false];
+
+//7) declare and initialized an array and store variableeducation in pakistan
+
+//let qualificationINPakistan = ["SSC" , "HSC" , "BSC" , "BS" , "BCOM" , "MS" , "M.Phill" , "phD"]
+//console.log(qualificationINPakistan);
+
+      // 8.initialized and declared the three variables and store students names ,their score and total marks
+
+      //let students = ["ANUM" , "HARAM" , "AYZal"];
+      //let score = [450 , 390 , 350];
+      //let tolatMarks = [500];
+      //let computePercentage  = (450/500*100) ;
+      //let computePercentage1 = (390/500*100);
+      //let computePercentage2 = (350/500*100) ;      
+      //document.write("score of " + students[0] + " is " + score[0] + ". Percentage: " + computePercentage + "<br/>");
+      //document.write("score of " + students[1] + " is " + score[1] + ". Percentage: " + computePercentage1 + "<br/>");
+      //document.write("score of " + students[2] + " is " + score[2] + ". Percentage: " + computePercentage2 + "<br/>");
+      
+         //9 . INItialize an array color name and display the array elemnt in your browser;
+
+    // let colors = [];
+         
+         // a. Ask user what color she wants to add
+
+         //let userColor= prompt("Enter color you want to add in the begining");
+         //colors.unshift(userColor);
+         //document.write("updated Array" + colors +  "<br/>");
+         //console.log(colors);
+
+          // b. Ask user what color she wants to add to the "END"
+        
+         //let userColor1 = prompt("Enter the desire colour you want to add at the end");
+         //colors.push(userColor1);
+         //document.write("updated Array" + colors +  "<br/>" );
+         //console.log(colors);
+
+         // c .add two more color in an array and update in your browser
+         //document.write(colors +  "<br/>" );
+         //let userColor3 = prompt("Enter 2 more color at the begining");
+         //colors.shift(userColor3);
+         //document.write( "updated Array" + colors +  "<br/>" );
+        // console.log(colors);
+
+    // 10 . Sort the score of the students
+
+         //let studentsScore =[320 , 230 , 480 , 120];
+        // studentsScore.sort();
+         //console.log(studentsScore);
+
+// 11 . 
+//var cities = ["Lahore" , "Islamabad ", "Quetta ", "Peshawar"];
+ //var selectCities = cities.slice(1, 3);
+ //console.log( cities);
+ //console.log( selectCities);
+
+    // 12. Write a program to write a single string from the below mentioned array
+
+          //var arr = ["This" , "is" , "my" , "cat"];
+          //var save = arr.join(' ');
+          //document.write(save)
+          //console.log(save);
+
+//      13 create  new array ,store values one by one in such a way that you canaccessthe value in order in which they were stored
+    
+
+     //      " FIRST IN FIRST OUT (METHOD)"
+                                                                     
+     //var compDevices = ["keyboard" , "Mouse" ,"Monitor", "Printer"];
+     //compDevices.pop();
+     //compDevices.pop();
+    // compDevices.pop();
+
+     //console.log (compDevices);
+
+ //var compDevices1 = ["Mouse", "keyboard" ,"Monitor", "Printer"];
+ //compDevices1.pop();
+ //compDevices1.pop();
+ //compDevices1.pop();
+ //console.log (compDevices1);
+
+ //var compDevices2 = [ "Printer", "Mouse", "keyboard" ,"Monitor"];
+ //compDevices2.pop();
+ //compDevices2.pop();
+ //compDevices2.pop();
+ //console.log (compDevices2);
+
+ //var compDevices3 = ["Monitor", "Printer", "Mouse", "keyboard" ];
+ //compDevices3.pop();
+ //compDevices3.pop();
+ //compDevices3.pop();
+ //console.log (compDevices3);
+
+//      14 . create  new array ,store values one by one in such a way that you canaccessthe value in order in which they were stored
+
+                    //LAST IN FIRST OUT METHOD
+//var parts = ["keyboard", "Mouse", "printer", "Monitor" ];
+//parts.shift();
+//parts.shift();
+//parts.shift();
+//console.log(parts);
+
+//var parts = ["keyboard", "Mouse", "Monitor" , "printer" ];
+//parts.shift();
+//parts.shift();
+//parts.shift();
+//console.log(parts)
+
+//var parts = ["keyboard", "printer", "Monitor" , "Mouse"];
+//parts.shift();
+//parts.shift();
+//parts.shift();
+//console.log(parts)
+
+//var parts = ["Mouse", "printer", "Monitor" , "keyboard"];
+//parts.shift();
+//parts.shift();
+//parts.shift();
+//console.log(parts)
+
+
+
+
+  //    15. show devices of mubile dropdown/selectmenu show  in browser
+
+            // var devices = ["Apple" ,"Samsung" ,"Nokia" , "Haier" , "Metrolla"];
+            //// var index = 0;
+            // document.write("<select>")
+            // document.write("<option>" +  devices[index++] + "<option>")
+            // document.write("<option>" +  devices[index++] + "<option>")
+            // document.write("<option>" +  devices[index++] + "<option>")
+            // document.write("<option>" +  devices[index++] + "<option>")
+            // document.write("<option>" +  devices[index++] + "<option>")
+            // document.write("<select>")
+
+             
+
+
+
+    
+         
+
